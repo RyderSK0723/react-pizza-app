b0115255b9b73aa84d077811877215a58b93e083
RyderSK0723 <lucky.star961117@gmail.com>
2025-01-25 02:29:46 -0500

    full_completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import PizzaForm from "./components/PizzaForm";
+import PizzaList from "./containers/PizzaList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PizzaForm />
+      <PizzaList />
+    </>
+  );
+}
+
+export default App;
